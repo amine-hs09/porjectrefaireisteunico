677539312e77e53dfafd087809ab05ba75fd23b9
amine-hs09 <146427409+amine-hs09@users.noreply.github.com>
2025-06-06 20:45:41 +0200

    Delete public directory

--- public/team.php
+++
@@ -1,4 +0,0 @@
-<?php include '../includes/header.php'; ?>
-<h2>Notre Équipe</h2>
-<p>Unico Consult a été fondé par <strong>Otman</strong> et <strong>Farid</strong>. Nous mettons nos valeurs de rigueur et d'innovation au service de votre entreprise.</p>
-<?php include '../includes/footer.php'; ?>
--- public/trust.php
+++
@@ -1,4 +0,0 @@
-<?php include '../includes/header.php'; ?>
-<h2>Ils nous font confiance</h2>
-<p>Maison'Net, La Cense et bien d'autres partenaires nous accordent leur confiance.</p>
-<?php include '../includes/footer.php'; ?>
